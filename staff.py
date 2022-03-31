6083f37ac59b44384d48f06f43528b0079127c18
ashensamarakkodige <ashen.samarakkodig@live.vu.edu.au>
2022-04-01 10:58:17 +1100

    get all salarys

--- staff.py
+++ staff.py
@@ -4,4 +4,12 @@ file = open('./employees.csv', 'r')
 
 reader = csv.reader(file)
 
-print(reader)
+managersCount = 0
+totSalary = 0
+
+
+
+for row in reader:
+    
+    if (row[2] != 'salary'):
+        print(row[2])
