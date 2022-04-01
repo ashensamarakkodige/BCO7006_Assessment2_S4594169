ee6e1692c665cd94fd7f635f4eb8561263216ca2
ashensamarakkodige <ashen.samarakkodig@live.vu.edu.au>
2022-04-01 11:07:22 +1100

    take salary and recodes separatly

--- staff.py
+++ staff.py
@@ -4,12 +4,16 @@ file = open('./employees.csv', 'r')
 
 reader = csv.reader(file)
 
+recodeArray = []
 managersCount = 0
 totSalary = 0
 
+arrSalary = []
+lowSalary = 0
 
 
 for row in reader:
     
     if (row[2] != 'salary'):
         print(row[2])
+        
